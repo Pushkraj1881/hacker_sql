1943885dd16d590e35c3184c360adfba2a6eab96
Pushkraj1881 <108886598+Pushkraj1881@users.noreply.github.com>
2025-03-27 21:44:56 +0530

    Create SELECT_QUERY2.sql

---
+++ SELECT_QUERY2.sql
@@ -0,0 +1,13 @@
+/* Query the NAME field for all American cities in the CITY table with populations larger than 120000. The CountryCode for America is USA.
+
+The CITY table is described as follows:
++----------+------------+------+------------+
+| ID       | Name       | CountryCode | District     | Population |
++----------+------------+-------------+-------------+------------+
+
+*/
+
+SELECT NAME
+FROM CITY
+WHERE POPULATION > 120000
+AND COUNTRYCODE = 'USA';
